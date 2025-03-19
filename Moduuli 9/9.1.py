888d8649b1d6d7c53aa2a437dcc93311b36b2a0e
LTaskinen <lauri.taskinen2@metropolia.fi>
2025-03-19 13:35:11 +0200

    uusi

---
+++ Moduuli 9/9.1.py
@@ -0,0 +1,8 @@
+class auto:
+    def __init__(self, register, topspd, currentspd=0, distance=0):
+        self.register = register
+        self.topspd = topspd
+        self.currentspd = currentspd
+        self.distance = distance
+auto1 = auto('ABC-123', 142)
+print(f'{auto1.register}, {auto1.topspd}, {auto1.currentspd}, {auto1.distance}')
